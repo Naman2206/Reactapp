4de9a03b6927c7eb93d6c4be203f915174684f68
Naman2206 <96406585+Naman2206@users.noreply.github.com>
2022-02-02 13:08:46 +0530

    set counter on gui useState is use for counter in gui

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
+import react, { useState } from "react";
 import "./styles.css";
 // var username = prompt("Give your user name");
 var color = "green";
 var username = "Naman";
-
+var clickcounter = 0;
 export default function App() {
+  const [clickcounter, setclickcounter] = useState(0);
   function clickeventhandler() {
-    console.log("clicked!");
+    var newclickcounter = clickcounter + 1;
+    setclickcounter(newclickcounter);
   }
   return (
     <div className="App">
       <h1 style={{ backgroundColor: color }}>Welcome {username}</h1>
       <button onClick={clickeventhandler}> Translate</button>
+      {clickcounter}
     </div>
   );
 }
