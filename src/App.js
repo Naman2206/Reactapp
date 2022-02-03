29b3b43cce65534b155e3b1a3f1c1fce92e8317b
Naman2206 <96406585+Naman2206@users.noreply.github.com>
2022-02-03 17:50:31 +0530

    creating list

--- src/App.js
+++ src/App.js
@@ -3,28 +3,18 @@ import "./styles.css";
 // var username = prompt("Give your user name");
 var color = "green";
 var username = "Naman";
-var emojiDictionary = {
-  "😀": " Grinning Face",
-  "😟": "Worried Face",
-  "😕": "Confused Face",
-  "🤭": "Face with Hand Over Mouth"
-};
+var shoppingList = ["milk", "Flowers", "Dhaniya", "lemon", "rava"];
 export default function App() {
-  const [meaning, setMeaning] = useState("");
-  function emojiinputhandler(event) {
-    var userinput = event.target.value;
-    var meaning = emojiDictionary[userinput];
-    if (meaning === undefined) {
-      meaning = " we don't have database";
-    }
-    setMeaning(meaning);
-  }
-
   return (
     <div className="App">
       <h1 style={{ backgroundColor: color }}>Welcome {username}</h1>
-      <input onChange={emojiinputhandler} />
-      <h2>Meaning:{meaning}</h2>
+      <h2>Shoping list</h2>
+      <ul>
+        {shoppingList.map((item) => {
+          console.log(item);
+          return <li>{item}</li>;
+        })}
+      </ul>
     </div>
   );
 }
